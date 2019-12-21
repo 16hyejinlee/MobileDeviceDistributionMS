ac9188b7449dbee0a0e1b3814fbbc422344d0830
tmxpffldk <tmxpffldk@naver.com>
2019-12-22 02:20:20 +0900

    Add DB data file

--- DB/option.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `option` (
-  `option_value` varchar(45) DEFAULT NULL,
-  `option_type` varchar(45) DEFAULT NULL,
-  `option_idx` int(11) NOT NULL,
-  PRIMARY KEY (`option_idx`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
